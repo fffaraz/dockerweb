0a2049e6b772458d871fdd4427d71e8318651b54
Faraz Fallahi <fffaraz@gmail.com>
2016-12-22 17:31:41 -0600

    python3

--- web/python3/script_init.sh
+++ web/python3/script_init.sh
@@ -5,9 +5,74 @@ groupadd webuser
 useradd --no-create-home --shell /bin/bash --gid webuser webuser
 
 apt-get update
-apt-get -yq install nano wget
-pip install uwsgi django gunicorn psycopg2 Flask
+apt-get -yq install ca-certificates nano nginx tree wget
+pip install django gunicorn psycopg2 uwsgi Flask
 
 setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/uwsgi
+setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/python3
 
+echo '
+uwsgi_param  QUERY_STRING       $query_string;
+uwsgi_param  REQUEST_METHOD     $request_method;
+uwsgi_param  CONTENT_TYPE       $content_type;
+uwsgi_param  CONTENT_LENGTH     $content_length;
+
+uwsgi_param  REQUEST_URI        $request_uri;
+uwsgi_param  PATH_INFO          $document_uri;
+uwsgi_param  DOCUMENT_ROOT      $document_root;
+uwsgi_param  SERVER_PROTOCOL    $server_protocol;
+uwsgi_param  REQUEST_SCHEME     $scheme;
+uwsgi_param  HTTPS              $https if_not_empty;
+
+uwsgi_param  REMOTE_ADDR        $remote_addr;
+uwsgi_param  REMOTE_PORT        $remote_port;
+uwsgi_param  SERVER_PORT        $server_port;
+uwsgi_param  SERVER_NAME        $server_name;
+' > /etc/nginx/uwsgi_params
+
+echo '
+user webuser;
+worker_processes 1;
+pid /run/nginx.pid;
+events {
+	worker_connections 1024;
+	multi_accept on;
+}
+http {
+	sendfile on;
+	tcp_nopush on;
+	tcp_nodelay on;
+	keepalive_timeout 65;
+	charset utf-8;
+	client_max_body_size 128M;
+	types_hash_max_size 2048;
+	include /etc/nginx/mime.types;
+	default_type application/octet-stream;
+	access_log /home/webuser/log/nginx/access.log;
+	error_log /home/webuser/log/nginx/error.log;
+	server {
+		listen 80 default_server;
+		location /media  {
+			alias /home/webuser/www/media;
+		}
+		location /static {
+			alias /home/webuser/www/static;
+		}
+		location = /favico.ico { alias /home/webuser/www/favico.ico; }
+		location / {
+			proxy_pass http://127.0.0.1:8000;
+			proxy_set_header Host             $host;
+			proxy_set_header X-Real-IP        $remote_addr;
+			proxy_set_header X-Forwarded-For  $proxy_add_x_forwarded_for;
+			proxy_set_header X-Forwarded-Host $server_name;
+
+			#include uwsgi_params;
+			#uwsgi_pass 127.0.0.1:8000;
+			#proxy_redirect off;
+		}
+	}
+}
+' > /etc/nginx/nginx.conf
+
+rm -rf /var/lib/apt/lists/*
 rm /script_init.sh
--- web/python3/script_run.sh
+++ web/python3/script_run.sh
@@ -2,13 +2,13 @@
 set -euxo pipefail
 
 mkdir -p /home/webuser/.ssh
-mkdir -p /home/webuser/log
+mkdir -p /home/webuser/log/nginx
 mkdir -p /home/webuser/tmp
 mkdir -p /home/webuser/www
 
 chmod -R 700 /home/webuser/.ssh
 
-[[ ! -f /script_run_aux.sh ]] && cat > /home/webuser/www/wsgi.py <<'EOL'
+[ ! -f /home/webuser/www/wsgi.py ] && cat > /home/webuser/www/wsgi.py <<'EOL'
 def application(environ, start_response):
 	start_response('200 OK', [('Content-Type', 'text/html')])
 	return [b"<h1 style='color:blue'>Hello There!</h1>"]
@@ -16,15 +16,17 @@ EOL
 
 chown -R webuser:webuser /home/webuser
 
+[ -f /home/webuser/www/requirements.txt ] && pip install -r /home/webuser/www/requirements.txt
+
 cd /home/webuser/www
 
-# --uid webuser --gid webuser
-# --workers 1 --threads 10
-exec uwsgi --socket 0.0.0.0:80 --protocol=http -w wsgi
+uwsgi --uid webuser --gid webuser --socket 127.0.0.1:8000 --protocol=http --wsgi-file wsgi.py &
+exec /usr/sbin/nginx
 
 #exec uwsgi --ini myapp.ini
-#exec gunicorn --bind 0.0.0.0:80 myproject.wsgi:application
-#exec ./manage.py runserver 0.0.0.0:80
+#exec uwsgi --json myapp.json
+#exec gunicorn --bind 127.0.0.1:8000 myproject.wsgi:application
+#exec python manage.py runserver 127.0.0.1:8000
 
-#python /app/manage.py collectstatic --noinput
-#/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app --log-level=DEBUG --timeout 600
+#python manage.py collectstatic --noinput
+#/usr/local/bin/gunicorn config.wsgi -w 4 -b 127.0.0.1:8000 --chdir=/app --log-level=DEBUG --timeout 600
