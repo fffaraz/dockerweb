73f770bdc81d96771e39a33d3710ba86fadced20
Faraz Fallahi <fffaraz@gmail.com>
2016-12-22 21:40:12 -0600

    install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# wget -qO - https://raw.githubusercontent.com/fffaraz/dockerweb/master/install.sh | bash
+
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
+set -euxo pipefail
+
+apt-get -yq update
+apt-get -yq install git
+
+mkdir -p /opt
+git clone https://github.com/fffaraz/dockerweb.git /opt/dockerweb
+export PATH=$PATH:/opt/dockerweb
+echo 'export PATH=$PATH:/opt/dockerweb' >> ~/.profile
+
+#docweb install:swapfile 1024
+docweb bootstrap
