f795769c7813422d964900b94117100412f6f765
Faraz Fallahi <fffaraz@gmail.com>
2018-03-23 10:15:55 -0500

    vbox

--- app/phpvirtualbox/script_init.sh
+++ app/phpvirtualbox/script_init.sh
@@ -19,15 +19,20 @@ var $password = 'pass';
 var $location = 'http://172.17.0.1:18083/';
 var $language = 'en';
 var $vrdeports = '9000-9100';
-var $vrdeaddress = '127.0.0.1';
+var $vrdeaddress = '127.0.0.1'; // '0.0.0.0'
+var $noAuth = true;
+var $previewUpdateInterval = 10;
+var $previewWidth = 240;
 var $maxProgressList = 5;
 var $deleteOnRemove = true;
 var $browserRestrictFiles = ['.iso','.vdi','.vmdk','.img','.bin','.vhd','.hdd','.ovf','.ova','.xml','.vbox','.cdr','.dmg','.ima','.dsk','.vfd'];
 var $hostMemInfoRefreshInterval = 5;
+var $vmMemoryStartLimitWarn = true;
+var $vmMemoryOffset = 1024;
+var $enableGuestAdditionsVersionDisplay = true;
 var $consoleResolutions = ['640x480','800x600','1024x768','1280x720','1440x900'];
 var $consoleKeyboardLayout = 'EN';
 var $nicMax = 4;
-var $noAuth = true;
 }
 EOL
 
