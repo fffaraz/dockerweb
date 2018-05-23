acc400679f1b9428be92e9794a5406c3d04a7c0d
Faraz Fallahi <fffaraz@gmail.com>
2018-05-23 15:08:42 -0500

    php:5-apache

--- web/php5apache/script_init.sh
+++ web/php5apache/script_init.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
 set -euxo pipefail
 
-# clean up
+apt-get -yq update
+
+apt-get -yq install git zip unzip php5-mcrypt php5-json php5-mysql
+apt-get -yq install libfreetype6-dev libjpeg62-turbo-dev libmcrypt-dev libpng-dev
+
+# PHP Core Extensions
+docker-php-ext-install -j$(nproc) iconv mcrypt
+docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
+docker-php-ext-install -j$(nproc) gd
+
+php5enmod mcrypt
+php5enmod json
+a2enmod rewrite
+service apache2 restart
+
+# Composer
+cd /tmp
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+composer global require "laravel/installer"
+
+# Site
+cat > /etc/apache2/sites-available/000-laravel.conf <<'EOL'
+<VirtualHost *:80>
+        DocumentRoot /home/webuser/www/public
+        <Directory />
+                Options FollowSymLinks
+                AllowOverride All
+        </Directory>
+        <Directory /home/webuser/www/>
+                Options Indexes FollowSymLinks MultiViews
+                AllowOverride All
+                Order allow,deny
+                allow from all
+        </Directory>
+        LogLevel warn
+        ErrorLog /home/webuser/log/apache/error.log
+        CustomLog /home/webuser/log/apache/access.log combined
+</VirtualHost>
+EOL
+
+/usr/sbin/a2dissite '*'
+/usr/sbin/a2ensite 000-laravel
+service apache2 restart
+
+# Clean up
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 rm /script_init.sh
--- web/php5apache/script_run.sh
+++ web/php5apache/script_run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
+
+mkdir -p /home/webuser/log/apache
+export PATH=$PATH:$HOME/.composer/vendor/bin
+
+exec apache2-foreground
