42cd07d1d65d93ce312f1b9fc5deb2368d11ac08
Faraz Fallahi <fffaraz@gmail.com>
2019-01-22 15:05:50 -0600

    Update script_init.sh

--- balancer/script_init.sh
+++ balancer/script_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
-export DEBIAN_FRONTEND=noninteractive
-apt update
-apt -y upgrade
-apt -y install bash nano wget
+#export DEBIAN_FRONTEND=noninteractive
+#apt update
+#apt -y upgrade
+#apt -y install bash nano wget
 
 echo "
 resolver 127.0.0.11 valid=30s ipv6=off;
