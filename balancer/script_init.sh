afeb0987a40a1abb836339744161c2246d501f9b
Faraz Fallahi <fffaraz@gmail.com>
2019-07-29 15:26:38 -0500

    Update script_init.sh

--- balancer/script_init.sh
+++ balancer/script_init.sh
@@ -9,7 +9,14 @@ set -euxo pipefail
 echo "
 resolver 127.0.0.11 valid=30s ipv6=off;
 resolver_timeout 5s;
-" >> /etc/nginx/conf.d/default.conf
+" > /etc/nginx/conf.d/resolver.conf
+
+echo "
+proxy_connect_timeout       600;
+proxy_send_timeout          600;
+proxy_read_timeout          600;
+send_timeout                600;
+" > /etc/nginx/conf.d/timeout.conf
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
