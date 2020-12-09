98f681aeaadbea75fd1b847bf5e0b400dab96511
Faraz Fallahi <fffaraz@gmail.com>
2020-12-08 21:36:00 -0500

    remove vbox

--- app/phpvirtualbox/Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM fffaraz/web/php7nginx:latest
-ADD script_*.sh /
-RUN chown root:root /script_*.sh && chmod 540 /script_*.sh && sync && /script_init.sh
--- app/phpvirtualbox/script_run_aux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /home/webuser/www/public/pvb ]; then
-	echo "<html><body><h1>It works!</h1></body></html>" > /home/webuser/www/public/index.html
-	ln -s /opt/phpvirtualbox /home/webuser/www/public/pvb
-fi
